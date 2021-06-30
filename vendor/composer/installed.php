0225f08ee4ead631ddb0918667ff4f217e4d0155
rbkjefkj <sneakysnayke@protonmail.com>
2021-06-30 10:48:46 +0300

    added sthg

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '95700413e3632071d0c7850c4b68502de16d43a3',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '95700413e3632071d0c7850c4b68502de16d43a3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
